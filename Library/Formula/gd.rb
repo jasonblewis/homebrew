b6c0249598c6f6215f06c6f70d5e38de82a0f716
Jason Lewis <jason@dickson.st>
2011-10-06 14:59:21 +1200

    moved  back to 2.0.33 which still uses ./configure as a stop gap measure

--- Library/Formula/gd.rb
+++ Library/Formula/gd.rb
@@ -6,12 +6,8 @@ class Gd < Formula
   # url "http://www.libgd.org/releases/gd-2.0.36RC1.tar.gz"
 
   homepage "http://bitbucket.org/pierrejoye/gd-libgd"
-  #url "http://download.osgeo.org/mapserver/libgd/gd-2.0.36RC1.tar.gz"
-  #url "https://bitbucket.org/pierrejoye/gd-libgd/get/GD_2_0_34RC1.tar.gz"
   url "https://bitbucket.org/pierrejoye/gd-libgd/get/GD_2_0_33.tar.gz"
   md5 "2ac0b29a3afebd44445bf5a3498eb2a4"
-  #md5 "b26a9134e90c03df9918442a01bcd4c4"
-  #md5 "39ac48e6d5e0012a3bd2248a0102f209"
   version "2.0.33"
   
   #head "http://bitbucket.org/pierrejoye/gd-libgd", :using => :hg
